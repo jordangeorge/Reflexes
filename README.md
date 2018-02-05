3593f16966c355bdec65761a08c966985b4fdfbc
Jordan George <jordanngeorge@gmail.com>
2018-02-05 15:51:57 -0500

    add README + other small changes

---
+++ README.md
@@ -0,0 +1,40 @@
+# Reflexes
+
+Test your reflexes in this exciting Raspberry Pi game designed with the intention of teaching kids about computer science and engineering. Made for Engineering Day (E-Day) 2017-2018 at the University of Kentucky.
+
+<hr>
+
+[Get started with your Raspberry Pi using this link](https://www.imore.com/how-get-started-using-raspberry-pi). You do not need an internet connection.
+
+Once you've completed getting started with the Raspberry Pi, you can set up the physical aspect of this project.
+
+What you'll need (in addition to the things from the link above):
+- Breadboard
+- 5 Male-to-Female jumper/DuPont wires
+- 4 Female-to-Female jumper/DuPont wires
+- 4 resistors
+- 3 LED lights, preferably all being different colors or one being a different color from the other two
+- Button
+
+Program was created using Raspberry Pi 3 Model B and Python 2.
+
+<img src="images/IMG_1978.jpg"
+  alt="Markdown Monster icon"
+  style="width:100px;" />
+<img src="images/IMG_1979.jpg"
+  alt="Markdown Monster icon"
+  style="width:100px;" />
+<img src="images/IMG_1980.jpg"
+  alt="Markdown Monster icon"
+  style="width:100px;" />
+<img src="images/IMG_1981.jpg"
+  alt="Markdown Monster icon"
+  style="width:100px;" />
+<img src="images/IMG_1982.jpg"
+  alt="Markdown Monster icon"
+  style="width:100px;" />
+<img src="images/IMG_1983.jpg"
+  alt="Markdown Monster icon"
+  style="width:100px;" />
+
+The average score is about .2 so getting less than that means that the player has great reflexes!
--- reflexes.py
+++ reflexes.py
@@ -1,59 +1,52 @@
 from gpiozero import LED, Button
-from time import sleep,time
+from time import sleep, time
 import time
 from random import randint, seed
 
+green = LED(25)
+blue = LED(24)
 red = LED(23)
-green = LED(24)
-blue = LED(25)
 
 button = Button(26)
+
 def run():
     count = 0
 
     print "Test Your Reflexes 1.0"
-    name = raw_input("What is your name?")
-    raw_input("Press any button when ready")
-    
+    name = raw_input("What is your name? ")
+    raw_input("Press ENTER on the keyboard when ready")
+
     seed()
-    one = randint(0,9)
+    one = randint(0, 9)
     seed()
-    two = randint(0,9)
+    two = randint(0, 9)
     seed()
+
     green.on()
     sleep(one)
     green.off()
 
-
     blue.on()
     sleep(two)
-    
     blue.off()
 
     red.on()
 
-
     start = time.time()
-    
+
     while True:
-        
         button.wait_for_press()
         finish = time.time()
         yourtime = finish - start
-        mystring =  "Hello " + name + " Your score is " + str(format(yourtime, '.10f'))
+        mystring =  "Hello, " + name + ". Your score is " + str(format(yourtime, '.5f'))
 
         print mystring
 
         red.off()
 
-        with open("scores.txt", "a") as myfile:
-            myfile.write("Name: " + name + "---------------- Score: " + str(format(yourtime, '.10f')))
+        with open("scores.txt", "a") as scoresFile:
+            entry = ("Name: " + name + "\t Score: " + str(format(yourtime, '.10f')) + "\n").expandtabs(30)
+            scoresFile.write(entry)
             break;
 
-        
-        
- 
-
-    
 run()
-    
