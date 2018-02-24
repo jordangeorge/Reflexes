f69e8d3ffeabffbdd6605c097ee4fe00c1d6c129
Jordan George <jordanngeorge@gmail.com>
2018-02-24 15:01:55 -0500

    add top scores file; allow people to play without having to rerun

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 Test your reflexes in this exciting Raspberry Pi game designed with the intention of teaching kids about computer science and engineering. Made for Engineering Day (E-Day) 2017-2018 at the University of Kentucky.
 
-Once the program is run, the player enters their name and pressed the 'enter' key to start the game. The player must then wait for the third/red LED to light up to press the button as quick as they can. The average score is about .2. Getting less than that means that the player has great reflexes!
+Once the program is run, the player enters their name and pressed the 'enter' key to start the game. The player must then wait for the third/red LED to light up to press the button as quick as they can.
+
+The average score is approximately 0.25. Getting less than 0.2 means that the player has great reflexes! Scores that are around 0.00006 are 'cheaters' because they're most likely holding the button down.
 
 <hr>
 
-[Get started with your Raspberry Pi using this link](https://www.imore.com/how-get-started-using-raspberry-pi). You do not need an internet connection.
+[Get started with your Raspberry Pi using this link](https://www.imore.com/how-get-started-using-raspberry-pi).
 
 Once you've completed getting started with the Raspberry Pi, you can set up the physical aspect of this project.
 
@@ -19,6 +21,8 @@ What you'll need (in addition to the things from the link above):
 - Button
 
 Program was originally created using the Raspberry Pi 3 Model B and Python 2.
+
+Use ```watch -n 1 tail scores.txt``` in the terminal to see additions to the scores.txt file live.
 
 <img src="images/IMG_1978.jpg" width="100">
 <img src="images/IMG_1979.jpg" width="100">
--- reflexes_python3.py
+++ reflexes_python3.py
@@ -2,6 +2,7 @@ from gpiozero import LED, Button
 from time import sleep, time
 import time
 from random import randint, seed
+from operator import itemgetter
 
 green = LED(25)
 blue = LED(24)
@@ -10,41 +11,60 @@ red = LED(23)
 button = Button(26)
 
 def run():
-    print("Test Your Reflexes 2.0")
-    name = input("What is your name? ") # saves player's name in the 'name' variable
-    input("Press ENTER on the keyboard when ready") # waits for player to press the enter key
+    while(1):
+        print("Test Your Reflexes 2.0")
+        name = input("What is your name? ") # saves player's name in the 'name' variable
+        input("Press ENTER on the keyboard when ready") # waits for player to press the enter key
 
-    seed()
-    one = randint(0, 9) # assigns random number to the 'one' variable
-    seed()
-    two = randint(0, 9) # assigns random number to the 'two' variable
-    seed()
+        seed()
+        one = randint(0, 9) # assigns random number to the 'one' variable
+        seed()
+        two = randint(0, 9) # assigns random number to the 'two' variable
+        seed()
 
-    green.on() # turn green LED on
-    sleep(one) # keep light on for value saved in 'one'
-    green.off() # turn green LED off
+        green.on() # turn green LED on
+        sleep(one) # keep light on for value saved in 'one'
+        green.off() # turn green LED off
 
-    blue.on()
-    sleep(two)
-    blue.off()
+        blue.on()
+        sleep(two)
+        blue.off()
 
-    red.on() # turn red LED on
+        red.on() # turn red LED on
 
-    start = time.time() # start timer
+        start = time.time() # start timer
 
-    while True:
-        button.wait_for_press() # wait for player to press button on breadboard
-        finish = time.time() # end timer
-        yourtime = finish - start # assigns time between red button turning on and player pressing button to 'yourtime'
-        mystring =  "Hello, " + name + ". Your score is " + str(format(yourtime, '.5f') + "!")
+        while True:
+            button.wait_for_press() # wait for player to press button on breadboard
+            finish = time.time() # end timer
+            yourtime = finish - start # assigns time between red button turning on and player pressing button to 'yourtime'
+            mystring =  "Hello, " + name + ". Your score is " + str(format(yourtime, '.5f') + "!")
 
-        print(mystring) # shows player their time/score
+            print(mystring) # shows player their time/score
 
-        red.off() # turn red LED off
+            red.off() # turn red LED off
 
-        with open("scores.txt", "a") as scoresFile: # open scores.txt; if there is no scores.txt file, create one
-            entry = ("Name: " + name + "\t Score: " + str(format(yourtime, '.5f')) + "\n").expandtabs(30)
-            scoresFile.write(entry) # save score to file
-            break; # end while loop
+            with open("scores.txt", "a") as scoresFile: # open scores.txt; if there is no scores.txt file, create one
+                entry = ("Name: " + name + "\t Score: " + str(format(yourtime, '.5f')) + "\n").expandtabs(30)
+                scoresFile.write(entry) # save score to file
+
+
+            with open("top-scores.txt", "w") as topScores: # create new top-scores text file
+                listOfLists = []
+
+                with open('scores.txt') as scoresFile:
+                    for line in scoresFile:
+                        lineList = line.strip('\n').split()
+                        listOfLists.append(lineList)
+
+                sortedListofLists = sorted(listOfLists, key=itemgetter(-1)) # sort by the last item in each list, which is the score of each player
+
+                for item in sortedListofLists:
+                    itemStr = ' '.join(item)
+                    topScores.write(itemStr)
+
+                break; # end while loop
+
+        print('\n')
 
 run()
